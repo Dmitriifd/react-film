b80af5955523a082a723dd21fab3f44e63f6f37b
mitriifd <idimka9@gmail.com>
2021-11-24 20:39:08 +0500

    добавление строки поиска

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import { Movies } from '../components/Movies';
+import React from 'react'
+import { Movies } from '../components/Movies'
+import { Preloader } from '../components/Preloader'
+import { Search } from '../components/Search'
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY
 
 class Main extends React.Component {
 	state = {
 		movies: [],
 	}
 
-    componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
-					.then((response) => response.json())
-					.then((data) => this.setState({ movies: data.Search }))
-    }
+	componentDidMount() {
+		fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+			.then((response) => response.json())
+			.then((data) => this.setState({ movies: data.Search }))
+	}
 
 	render() {
-		const { movies } = this.state;
+		const { movies } = this.state
 		return (
 			<main className='container content'>
-				{
-                    movies.length ? 
-                    (<Movies movies={this.state.movies} />) :
-                    <h3>Загрузка...</h3>
-                }
-				
+                <Search />
+				{movies.length ? (
+					<Movies movies={this.state.movies} />
+				) : (
+					<Preloader />
+				)}
 			</main>
 		)
 	}
