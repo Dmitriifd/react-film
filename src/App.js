9e41f8698ae06e18324abbd8c8cabcf2e83c27f3
mitriifd <idimka9@gmail.com>
2021-11-24 13:39:14 +0500

    состояние классового компонента и управление им

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends Component {
+
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     count: 0,
+  //     someKey: false
+  //   }
+  // }
+
+  // state может быть без конструктора
+  state = {
+    count: 0,
+    someKey: false
+  }
+
+  handleClickPlus = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  handleClickMinus = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleClickMinus}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.handleClickPlus}>+</button>
+      </div>
+    );
+  }
+
+  
 }
 
 export default App;
