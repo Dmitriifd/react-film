d1da5a82ec0566de2809e7755d099d4d8b5419fb
mitriifd <idimka9@gmail.com>
2021-11-24 20:11:03 +0500

    создание общего списка фильмов

---
+++ src/components/Movies.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Movie } from './Movie';
+
+const Movies = (props) => {
+    const {movies} = props;
+
+    return (
+        <div className='movies'>
+            {movies.map(movie => {
+                return <Movie key={movie.imdbID} {...movie} />
+            })}
+        </div>
+	)
+    
+};
+
+export {Movies};
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Movies } from '../components/Movies';
 
-const Main = () => {
-    return (
-        <main className="container content">
-            Lorem ipsum dolor sit amet.
-        </main>
-    );
-};
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-export {Main};
+class Main extends React.Component {
+	state = {
+		movies: [],
+	}
+
+    componentDidMount() {
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+					.then((response) => response.json())
+					.then((data) => this.setState({ movies: data.Search }))
+    }
+
+	render() {
+		const { movies } = this.state;
+		return (
+			<main className='container content'>
+				{
+                    movies.length ? 
+                    (<Movies movies={this.state.movies} />) :
+                    <h3>Загрузка...</h3>
+                }
+				
+			</main>
+		)
+	}
+}
+
+export { Main }
