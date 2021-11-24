4a09bb606e6d9c30d5e3f6a256dfee1002ecc376
mitriifd <idimka9@gmail.com>
2021-11-24 14:31:00 +0500

    map и атрибут key

--- src/App.js
+++ src/App.js
@@ -4,61 +4,21 @@ import React, {Component} from 'react'
 
 class App extends Component {
 
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     count: 0,
-  //     someKey: false
-  //   }
-  // }
-
-  // state может быть без конструктора
   state = {
-    count: 0,
-    isCounting: false
+    post: [
+      {id: 'abc1', name: 'JS Basic'},
+      {id: 'abc2', name: 'JS Advanced'},
+      {id: 'abc3', name: 'React JS'},
+    ]
   }
 
-  handleStart = () => {
-    this.setState({isCounting: true})
-    this.counterId = setInterval(() => {
-      this.setState({count: this.state.count + 1})
-    }, 1000)
-  }
-  handleStop = () => {
-    this.setState({ isCounting: false })
-    clearInterval(this.counterId)
-  }
-  handleReset = () => {
-    this.setState({ isCounting: false, count: 0 })
-    clearInterval(this.counterId)
-  }
-
-  componentDidMount() {
-    const userCount = localStorage.getItem('timer')
-    if (userCount) {
-      this.setState({ count: +userCount })
-    }
-  }
-
-  componentDidUpdate() {
-    localStorage.setItem('timer', this.state.count)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.counterId)
-  }
 
   render() {
     return (
       <div className="App">
-       <h1>React Timer</h1>
-       <h3>{this.state.count}</h3>
-       {!this.state.isCounting ? (
-         <button onClick={this.handleStart}>Start</button>)
-       : (<button onClick={this.handleStop}>Stop</button>
-       )}
-       <button onClick={this.handleReset}>Reset</button>
+        {this.state.post.map(post => (
+          <h2 key={post.id}>{post.name}</h2>
+        ))}
       </div>
     );
   }
