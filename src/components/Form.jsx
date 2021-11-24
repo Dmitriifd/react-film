9bad6d464407469a07b3f43ceab19714c143014a
mitriifd <idimka9@gmail.com>
2021-11-24 16:13:58 +0500

    валидация значений формы

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,6 +11,18 @@ class Form extends React.Component {
         this.setState({ [event.target.name]: event.target.value})
     }
 
+    validateName = () => {
+        if (this.state.firstName.length < 5) {
+            
+            alert('Короткое имя')
+        }
+    }
+    validateEmail = () => {
+        if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.state.email)) {
+			alert('Email не валидный')
+		}
+    }
+
 	render() {
         const { firstName, email } = this.state
 
@@ -22,6 +34,7 @@ class Form extends React.Component {
 					placeholder='firstName'
 					value={firstName}
 					onChange={this.handleChange}
+					onBlur={this.validateName}
 				/>
 				<input
 					type='email'
@@ -29,6 +42,7 @@ class Form extends React.Component {
 					placeholder='email'
 					value={email}
 					onChange={this.handleChange}
+					onBlur={this.validateEmail}
 				/>
 			</div>
 		)
